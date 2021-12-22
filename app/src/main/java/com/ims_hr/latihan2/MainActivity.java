7cd632ecbc9abb3a873d6e5c2447cd9bc340056a
Irsyad Purbha <irsyadpurbha@gmail.com>
2021-12-22 21:07:46 +0700

    UPDATE 20211222 21:07

--- app/src/main/java/com/ims_hr/latihan2/MainActivity.java
+++ app/src/main/java/com/ims_hr/latihan2/MainActivity.java
@@ -1,7 +1,8 @@
 package com.ims_hr.latihan2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
